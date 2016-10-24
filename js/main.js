81be8381d8a8a3818c6c3a72528e4da50809d468
georgesydney <hello@georgesydney.com>
2016-10-24 11:18:59 +0100

    Added jquery for menu and cycle plugin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-console.log('it works');
+$(document).ready(function(){
+
+	$( window ).resize(function() {
+  		location.reload();
+	});
+
+	$('.slideshow').cycle({
+
+	speed: 1000,
+	timeout: 0, 
+	fx: 'scrollHorz',
+	next: '.next',
+	prev: '.previous',
+
+	});
+
+	$('.captions').cycle({
+
+	speed: 250,
+	timeout: 0, 
+	fx: 'fade',
+	next: '.next',
+	prev: '.previous',
+
+	});
+
+	$('.plus').on('click', function(){
+
+		if ($('.plus').hasClass('active')) {
+
+		$('.about-panel').removeClass('visible');
+		$('.plus').removeClass('active');
+
+		} else {
+
+			$('.about-panel').addClass('visible');
+			$('.plus').addClass('active');
+
+		}
+
+	});
+
+});
