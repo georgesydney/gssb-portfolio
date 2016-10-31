ae98c229e2a8b496f725fbd57c08bf0ca21bc6e9
georgesydney <hello@georgesydney.com>
2016-10-31 18:20:34 +0000

    added social media icons

--- js/main.js
+++ js/main.js
@@ -1,39 +1,15 @@
 $(document).ready(function(){
 
-	$( window ).resize(function() {
-  		location.reload();
-	});
-
-	$('.slideshow').cycle({
-
-	speed: 1000,
-	timeout: 0, 
-	fx: 'scrollHorz',
-	next: '.next',
-	prev: '.previous',
-
-	});
-
-	$('.captions').cycle({
-
-	speed: 250,
-	timeout: 0, 
-	fx: 'fade',
-	next: '.next',
-	prev: '.previous',
-
-	});
-
 	$('.plus').on('click', function(){
 
 		if ($('.plus').hasClass('active')) {
 
-		$('.about-panel').removeClass('visible');
+		$('.about-panel').removeClass('visible-side');
 		$('.plus').removeClass('active');
 
 		} else {
 
-			$('.about-panel').addClass('visible');
+			$('.about-panel').addClass('visible-side');
 			$('.plus').addClass('active');
 
 		}
