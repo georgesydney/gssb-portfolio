3c12376638322843f0a47740fd5c2740fc5639a9
chloewinstanley <chloelouisewinstanley@gmail.com>
2019-05-03 00:22:20 +0100

    testing random position on mobile

--- js/main.js
+++ js/main.js
@@ -4,49 +4,49 @@ $(document).ready(function(){
         $(this).css({"left": Math.random() * window.outerWidth , "top": Math.random() * window.outerHeight});
 	});
 
-	$.fn.draggable = function(){
-		    var $this = this,
-		    ns = 'draggable_'+(Math.random()+'').replace('.',''),
-		    mm = 'mousemove.'+ns,
-		    mu = 'mouseup.'+ns,
-		    $w = $(window),
-		    isFixed = ($this.css('position') === 'fixed'),
-		    adjX = 0, adjY = 0;
+	// $.fn.draggable = function(){
+	// 	    var $this = this,
+	// 	    ns = 'draggable_'+(Math.random()+'').replace('.',''),
+	// 	    mm = 'mousemove.'+ns,
+	// 	    mu = 'mouseup.'+ns,
+	// 	    $w = $(window),
+	// 	    isFixed = ($this.css('position') === 'fixed'),
+	// 	    adjX = 0, adjY = 0;
 
-		    $this.mousedown(function(ev){
-		        var pos = $this.offset();
-		        if (isFixed) {
-		            adjX = $w.scrollLeft(); adjY = $w.scrollTop();
-		        }
-		        var ox = (ev.pageX - pos.left), oy = (ev.pageY - pos.top);
-		        $this.data(ns,{ x : ox, y: oy });
-		        $w.on(mm, function(ev){
-		            ev.preventDefault();
-		            ev.stopPropagation();
-		            if (isFixed) {
-		                adjX = $w.scrollLeft(); adjY = $w.scrollTop();
-		            }
-		            var offset = $this.data(ns);
-		            $this.css({left: ev.pageX - adjX - offset.x, top: ev.pageY - adjY - offset.y});
-		        });
-		        $w.on(mu, function(){
-		            $w.off(mm + ' ' + mu).removeData(ns);
-		        });
-		    });
+	// 	    $this.mousedown(function(ev){
+	// 	        var pos = $this.offset();
+	// 	        if (isFixed) {
+	// 	            adjX = $w.scrollLeft(); adjY = $w.scrollTop();
+	// 	        }
+	// 	        var ox = (ev.pageX - pos.left), oy = (ev.pageY - pos.top);
+	// 	        $this.data(ns,{ x : ox, y: oy });
+	// 	        $w.on(mm, function(ev){
+	// 	            ev.preventDefault();
+	// 	            ev.stopPropagation();
+	// 	            if (isFixed) {
+	// 	                adjX = $w.scrollLeft(); adjY = $w.scrollTop();
+	// 	            }
+	// 	            var offset = $this.data(ns);
+	// 	            $this.css({left: ev.pageX - adjX - offset.x, top: ev.pageY - adjY - offset.y});
+	// 	        });
+	// 	        $w.on(mu, function(){
+	// 	            $w.off(mm + ' ' + mu).removeData(ns);
+	// 	        });
+	// 	    });
 
-		    return this;
-		};
+	// 	    return this;
+	// 	};
 
-		$('#benchmark').draggable();
-		$('#rida').draggable();
-		$('#benchmark-logo').draggable();
-		$('#flag').draggable();
-		$('#killington-fall').draggable();
-		$('#na-detail').draggable();
-		$('#curator-author').draggable();
-		$('#prescient').draggable();
-		$('#hole-and-corner').draggable();
-		$('#not-reading-but-dancing').draggable();
+	// 	$('#benchmark').draggable();
+	// 	$('#rida').draggable();
+	// 	$('#benchmark-logo').draggable();
+	// 	$('#flag').draggable();
+	// 	$('#killington-fall').draggable();
+	// 	$('#na-detail').draggable();
+	// 	$('#curator-author').draggable();
+	// 	$('#prescient').draggable();
+	// 	$('#hole-and-corner').draggable();
+	// 	$('#not-reading-but-dancing').draggable();
 
 
 
